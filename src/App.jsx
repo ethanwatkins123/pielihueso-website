e312aa41418ed64c417ed2082873ed38b02e9665
ethanwatkins123 <ethanwatkins1000@gmail.com>
2024-04-16 09:49:49 -0300

    project initialised with styles

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles/styles.scss";
+
+function App() {
+  return (
+    <>
+      <h1>test header 1</h1>
+      <h2>test header 2</h2>
+      <p>test paragraph</p>
+    </>
+  );
+}
+
+export default App;
